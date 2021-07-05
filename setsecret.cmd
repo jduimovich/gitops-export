08774df04b8f2365d7168a9ffa1ddcad5efcde2e
John Duimovich <$MY_GITHUB_EMAIL>
2021-07-05 12:50:37 -0400

    fix scripts

---
+++ setsecret.cmd
@@ -0,0 +1,19 @@
+
+@echo off
+oc whoami --show-token >openshift.login.token
+set /P LOGINQ=< openshift.login.token
+del openshift.login.token
+Set LOGIN=%LOGINQ:"=%
+
+oc whoami --show-server >openshift.server
+set /P SERVERQ=< openshift.server
+del openshift.server
+Set SERVER=%SERVERQ:"=%
+echo  The server is /%SERVER%/ 
+rem echo  The token is  /%LOGIN%/
+ 
+gh secret set OPENSHIFT_SERVER -b %SERVER%
+gh secret set OPENSHIFT_TOKEN -b %LOGIN%
+gh secret set OPENSHIFT_NAMESPACE -b gitops-export
+
+:end
