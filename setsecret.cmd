bddbc0b3bbee95fc9f992408ef84e9060ecb525e
John Duimovich <jduimovich@gmail.com>
2021-07-07 14:38:32 -0400

    set params

--- setsecret.cmd
+++ setsecret.cmd
@@ -9,11 +9,18 @@ oc whoami --show-server >openshift.server
 set /P SERVERQ=< openshift.server
 del openshift.server
 Set SERVER=%SERVERQ:"=%
-echo  The server is /%SERVER%/ 
+echo The server is /%SERVER%/ 
 rem echo  The token is  /%LOGIN%/
- 
+
+oc project -q >tmp.ns
+set /P NS=< tmp.ns   
+Set NS=%NS: =%
+del tmp.ns
+
+echo Namespace for export is "%NS%"
+
 gh secret set OPENSHIFT_SERVER -b %SERVER%
 gh secret set OPENSHIFT_TOKEN -b %LOGIN%
-gh secret set OPENSHIFT_NAMESPACE -b gitops-export
+gh secret set OPENSHIFT_NAMESPACE -b %NS%
 
 :end
